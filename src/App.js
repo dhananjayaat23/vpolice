782b0d483a6dc21c995a5436cdb00b527d58f465
dhananjayaat23 <dhananjayaaththanayaka23@gmail.com>
2024-11-18 17:47:29 +0530

    routing

--- src/App.js
+++ src/App.js
@@ -5,6 +5,11 @@ import Signup from './Screens/Signup';
 import Home from './Screens/Home';
 import AdminLogin from './Screens/AdminLogin';
 import AdminHome from './Screens/AdminHome';
+import Dashboard from './Screens/Dashboard';
+import ApprovedIncidents from './Screens/ApprovedIncidents'
+import DeclinedIncidents from './Screens/DeclinedIncidents';
+import UserRegistrationRequests from './Screens/UserRegistrationRequests';
+import Users from './Screens/Users';
 
 function App() {
   return (
@@ -16,6 +21,11 @@ function App() {
         <Route path="/home" element={<Home />} />
         <Route path="/adminlogin" element={<AdminLogin />} />
         <Route path="/adminhome" element={<AdminHome />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/approvedincidents" element={<ApprovedIncidents />} />
+        <Route path="/declinedincidents" element={<DeclinedIncidents />} />
+        <Route path="/registrationrequests" element={<UserRegistrationRequests />} />
+        <Route path="/users" element={<Users/>} />
       </Routes>
     </BrowserRouter>
   )
